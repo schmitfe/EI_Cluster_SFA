86e50982084ac8a20f98f8368c415e6e0ae7cfb2
schmitfe <felix.schmitt@uni-koeln.de>
2023-01-26 16:33:41 +0100

    New function for plotting raster plot of experiment with markers

---
+++ Source/Helper/Plot_Function.py
@@ -0,0 +1,35 @@
+import pickle
+import matplotlib.pyplot as plt
+import numpy as np
+
+#function which opens the output file and plots the spikes and stimulus protocol
+def plotExperiment (output_path, dpi=320, ax=None, labels=True):
+    with open(output_path, 'rb') as outfile:
+        spikes = pickle.load(outfile)
+        _ = pickle.load(outfile)
+        StimulusDict = pickle.load(outfile)
+        _ = pickle.load(outfile)
+    Marker=StimulusDict['Marker']
+    if ax is None:
+        fig, ax = plt.subplots(dpi=dpi)
+        #dark  color of spikes
+    ax.plot(spikes[0], spikes[1], '.',color='black', label='_nolegend_', markersize=0.5)
+    for Mark in Marker:
+        ax.axvline(Mark[0], color='g')
+        ax.axvline(Mark[0]+StimulusDict['PreperatoryDuration'], color='r')
+        ax.axvline(Mark[0]+StimulusDict['PreperatoryDuration']+StimulusDict['StimulusDuration'], color='blue')
+
+    if labels:
+        ax.set_xlabel('Time (ms)')
+        ax.set_ylabel('Neuron ID')
+        ax.set_title('Spike raster plot')
+        ax.legend(['PS', 'RS', 'TE'])
+    return ax
+
+
+
+# main function to test the plot function
+if __name__ == '__main__':
+
+    ax = plotExperiment('../Simulate_NEST/Data.pkl')
+    plt.show()
